e4bea1b4db1e05c9da22b34aa3d24eff9231ca69
MinhTruongHMT <ninjarua2118@gmail.com>
2023-11-30 13:59:50 +0700

    :fire:first commit

---
+++ app/src/main/java/stu/edu/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package stu.edu.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuItemCompat;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mnu_main, menu);
+        return true;
+    }
+}
